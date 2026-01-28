8aa76e50fc280e3edb1b499cf80fd633ef36df1d
mahoozi97 <mahoozi97@icloud.com>
2026-01-28 20:55:23 +0300

    dotenv configuration

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const methodOverride = require("method-override");
 const morgan = require("morgan");
 const session = require("express-session");
 const helmet = require("helmet");
-const dotenv = require("dotenv").config();
+require("dotenv").config();
 const PORT = process.env.PORT;
 const MONGODB_URL = process.env.MONGODB_URL;
 const SESSION_SECRET = process.env.SESSION_SECRET;
