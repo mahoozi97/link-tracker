d2a169a7f14d0bab226217f0db09091fbec50c2b
mahoozi97 <mahoozi97@icloud.com>
2026-02-01 22:08:43 +0300

    nodemailer2

--- utils/mailer.js
+++ utils/mailer.js
@@ -3,10 +3,9 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "Gmail",
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL,
     pass: process.env.PASS,
